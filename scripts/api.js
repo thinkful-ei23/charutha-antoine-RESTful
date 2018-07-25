02ef1ebaf45640418a058b46f228b19a1f692e79
Charutha Bandara <charuthabandara@gmail.com>
2018-07-25 11:25:51 -0400

    getItems()

--- scripts/api.js
+++ scripts/api.js
@@ -2,7 +2,8 @@ const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/charutha-antoine';
 
   const getItems = function(callback) {
-    return callback('api module works!');
+    $.getJSON(`${BASE_URL}/items`, callback)
+   // return callback('api module works!');
   }
 
   return {
